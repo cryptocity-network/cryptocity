d8688f45606d2e636ef279cf850e0bf6fba0824f
Jon Snow <jonny96snow@googlemail.com>
2024-01-31 12:08:42 +0000

    Update global grid

--- components/CountryCard.vue
+++ components/CountryCard.vue
@@ -7,18 +7,7 @@
     <div class="absolute left-0 top-0 size-full bg-gradient-to-t from-blue-darker to-transparent transition-opacity group-hover:opacity-0" />
     <div class="absolute left-0 top-0 size-full bg-blue-darker/5 opacity-0 transition-opacity group-hover:opacity-100" />
 
-    <FadeInImage
-      v-if="logo"
-      :image-url="logo.url"
-      class="absolute left-1/2 top-1/2 w-4/5 -translate-x-1/2 -translate-y-1/2"
-    />
-    <!-- <transition name="fade">
-            <img
-              v-show="loaded"
-              :src="logo.url"
-              @load="onLoaded"
-            >
-          </transition> -->
+    <DynamicLogo :custom-url="url" text-color="white" logo-color="white" class="absolute left-1/2 top-1/2 w-4/5 max-w-[300px] -translate-x-1/2 -translate-y-1/2 2xl:max-w-[400px]" />
   </ClientOnly>
 </template>
 
@@ -28,8 +17,8 @@ defineProps({
     type: Object,
     default: null
   },
-  logo: {
-    type: Object,
+  url: {
+    type: String,
     default: null
   }
 })
--- components/GlobalCarousel.vue
+++ components/GlobalCarousel.vue
@@ -1,47 +1,69 @@
 <template>
   <div
     v-if="!error && data"
-    ref="scroller"
-    class="no-scrollbar grid h-screen grid-rows-1 gap-16 overflow-x-auto bg-white px-16 pb-16 pt-104 xl:gap-24 xl:px-24 xl:pb-24"
-    :style="`grid-template-columns: repeat(${data.global?.countries?.countries.length + 2}, min-content)`"
+    class="relative h-screen  !px-0 pt-72"
+    :class="{'mx-auto': gridSmallerThanWindow}"
+    style="width: 100vw"
   >
-    <nuxt-link
-      v-for="country in data.global?.countries?.countries"
-      :key="country.id"
-      :to="country.url"
-      class="group relative aspect-[4/12] h-full min-w-[328px] overflow-hidden rounded-8 transition-transform hover:-translate-y-16 xl:aspect-[3/4]"
-    >
-      <CountryCard
-        v-if="country.socialLinks"
-        :image="country.socialLinks.image"
-        :logo="country.socialLinks.logo"
-      />
-    </nuxt-link>
-  </div>
-  <div v-if="!gridSmallerThanWindow">
-    <button
-      class="hocus:bg-blue-dark/30 absolute left-32 top-1/2 z-10 -mt-24 hidden size-48 cursor-pointer items-center justify-center rounded bg-blue-dark/20 text-white transition-[background-color] active:bg-blue-dark/40 sm:flex"
-      @click="slide('back')"
-    >
-      <svg width="16" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M1 12c0-.66.27-1.3.77-1.73L12.97.43a1.85 1.85 0 0 1 2.6.23c.63.77.56 1.89-.16 2.56l-9.78 8.6a.25.25 0 0 0-.02.35l.02.02 9.77 8.6a1.85 1.85 0 0 1-2.45 2.77L1.77 13.73A2.3 2.3 0 0 1 1 12Z"
-          fill="currentColor"
-        />
-      </svg>
-    </button>
+    <div v-if="data.global?.countries?.countries.length < 2" class="relative size-full">
+      <div
+        ref="scroller"
+        class="no-scrollbar relative grid h-full grid-rows-1 gap-16 overflow-x-auto bg-white p-16 xl:gap-24 xl:p-24"
+        :style="`grid-template-columns: repeat(${data.global?.countries?.countries.length}, min-content)`"
+      >
+        <nuxt-link
+          v-for="country in data.global?.countries?.countries"
+          :key="country.id"
+          :to="country.url"
+          class="group relative aspect-[4/12] h-full min-w-[328px] overflow-hidden rounded-8 transition-transform hover:-translate-y-16 xl:aspect-[3/4]"
+        >
+          <CountryCard
+            v-if="country.socialLinks"
+            :image="country.socialLinks.image"
+            :url="country.url"
+          />
+        </nuxt-link>
+      </div>
+      <button
+        v-if="!gridSmallerThanWindow"
+        class="hocus:bg-blue-dark/30 absolute left-32 top-1/2 z-10 hidden size-48 -translate-y-1/2 cursor-pointer items-center justify-center rounded bg-blue-dark/20 text-white transition-[background-color] active:bg-blue-dark/40 sm:flex"
+        @click="slide('back')"
+      >
+        <svg width="16" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M1 12c0-.66.27-1.3.77-1.73L12.97.43a1.85 1.85 0 0 1 2.6.23c.63.77.56 1.89-.16 2.56l-9.78 8.6a.25.25 0 0 0-.02.35l.02.02 9.77 8.6a1.85 1.85 0 0 1-2.45 2.77L1.77 13.73A2.3 2.3 0 0 1 1 12Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
 
-    <button
-      class="hocus:bg-blue-dark/30 absolute right-32 top-1/2 z-10 -mt-24 hidden size-48 cursor-pointer items-center justify-center rounded bg-blue-dark/20 text-white transition-[background-color] active:bg-blue-dark/40 sm:flex"
-      @click="slide('forward')"
-    >
-      <svg width="16" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" class="-mr-4 rotate-180">
-        <path
-          d="M1 12c0-.66.27-1.3.77-1.73L12.97.43a1.85 1.85 0 0 1 2.6.23c.63.77.56 1.89-.16 2.56l-9.78 8.6a.25.25 0 0 0-.02.35l.02.02 9.77 8.6a1.85 1.85 0 0 1-2.45 2.77L1.77 13.73A2.3 2.3 0 0 1 1 12Z"
-          fill="currentColor"
+      <button
+        v-if="!gridSmallerThanWindow"
+        class="hocus:bg-blue-dark/30 absolute right-32 top-1/2 z-10 hidden size-48 -translate-y-1/2 cursor-pointer items-center justify-center rounded bg-blue-dark/20 text-white transition-[background-color] active:bg-blue-dark/40 sm:flex"
+        @click="slide('forward')"
+      >
+        <svg width="16" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" class="-mr-4 rotate-180">
+          <path
+            d="M1 12c0-.66.27-1.3.77-1.73L12.97.43a1.85 1.85 0 0 1 2.6.23c.63.77.56 1.89-.16 2.56l-9.78 8.6a.25.25 0 0 0-.02.35l.02.02 9.77 8.6a1.85 1.85 0 0 1-2.45 2.77L1.77 13.73A2.3 2.3 0 0 1 1 12Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </div>
+    <div v-else class="grid size-full grid-flow-col grid-rows-1 gap-16  p-16 xl:gap-24 xl:p-24">
+      <nuxt-link
+        v-for="country in data.global?.countries?.countries"
+        :key="country.id"
+        :to="country.url"
+        class="group relative size-full overflow-hidden rounded-8 transition-transform hover:-translate-y-16 xl:aspect-[3/4]"
+      >
+        <CountryCard
+          v-if="country.socialLinks"
+          :image="country.socialLinks.image"
+          :url="country.url"
         />
-      </svg>
-    </button>
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
@@ -62,7 +84,7 @@ function slide (direction: string) {
 }
 const gridSmallerThanWindow: Ref<boolean> = ref(false)
 const checkGridSize = () => {
-  gridSmallerThanWindow.value = scroller.value?.scrollWidth === window?.innerWidth
+  gridSmallerThanWindow.value = scroller.value?.scrollWidth <= window?.innerWidth
 }
 
 onMounted(() => {
