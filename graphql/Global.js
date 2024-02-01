4e2d42a4eb039077b74baef0d6b037f7da4304b3
Jon Snow <jonny96snow@googlemail.com>
2024-02-01 13:47:06 +0000

    Remove region carousel from global

--- graphql/Global.js
+++ graphql/Global.js
@@ -2,25 +2,6 @@ export default () => {
   return `query {
     global {
       tagLine
-      regionsCarousel {
-        _modelApiKey
-        settings {
-          backgroundColor
-        }
-        countries{
-          id
-          url
-          name
-          socialLinks {
-            image {
-              url
-            }
-            logo {
-              url
-            }
-          }
-        }
-      }
     }
   }`
 }
