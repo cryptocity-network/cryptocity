6538e99c908a8cf48a635cdaf803cca0ca64ed60
Jon Snow <jonny96snow@googlemail.com>
2024-02-06 14:47:56 +0000

    Firefox bug fixes

--- components/RegionCard.vue
+++ components/RegionCard.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link
+    class="custom-ease group relative block aspect-[3/4] h-full min-h-full overflow-hidden rounded-8 transition-transform hover:-translate-y-16 xl:aspect-[3/4]"
     :to="region.url"
-    class="custom-ease group relative size-full overflow-hidden rounded-8 transition-transform hover:-translate-y-16 xl:aspect-[3/4]"
   >
     <FadeInImage
       :image-url="region.mainImage.url"
@@ -10,7 +10,7 @@
     <div class="absolute left-0 top-0 size-full bg-blue opacity-20 mix-blend-hard-light" />
     <div class="absolute left-0 top-0 size-full bg-gradient-to-t from-blue to-transparent opacity-60 transition-opacity group-hover:opacity-0" />
 
-    <DynamicLogo :custom-url="region.url" text-color="white" logo-color="white" class="absolute left-1/2 top-1/2 w-[215px] -translate-x-1/2 -translate-y-1/2 xl:w-[291px]" />
+    <DynamicLogo :custom-url="region.url" text-color="white" logo-color="white" class="absolute left-1/2 top-1/2 h-56 -translate-x-1/2 -translate-y-1/2 xl:h-72" />
   </nuxt-link>
 </template>
 
