16e08b21d84560ceaea4bbe8aaec9a0d68df6fb5
Jon Snow <jonny96snow@googlemail.com>
2024-02-07 10:48:04 +0000

    Update
    Zig zag image sizings
    Logos opacity
    Events carousel alignment

--- components/datocms-blocks/ZigZagContent.vue
+++ components/datocms-blocks/ZigZagContent.vue
@@ -9,15 +9,15 @@
       <li
         v-for="(item, i) in data.blocks"
         :key="i"
-        class="flex flex-col items-stretch gap-x-64 px-16 sm:px-24"
+        class="flex flex-col items-stretch gap-x-64 px-16 sm:px-24 lg:items-center"
         :class="{
-          'border-b border-blue-dark/10 pb-64 xl:pb-72':
+          'border-b border-blue-dark/10 pb-64 lg:pb-72':
             i === 0 && data.blocks.length > 1,
-          'border-b border-blue-dark/10 py-64 xl:py-72':
+          'border-b border-blue-dark/10 py-64 lg:py-72':
             i > 0 && i < data.blocks.length - 1,
-          'pt-64 xl:pt-72': i === data.blocks.length - 1,
-          'xl:flex-row-reverse': i % 2 === 0,
-          'xl:flex-row': i % 2 !== 0,
+          'pt-64 lg:pt-72': i === data.blocks.length - 1,
+          'lg:flex-row-reverse': i % 2 === 0,
+          'lg:flex-row': i % 2 !== 0,
         }"
       >
         <div v-if="item.image.video !== null" class="max-xl:h-360 child:md:max-xl:w-[calc(100vw-64px*2)]  relative flex justify-center child:mx-auto child:rounded-6 child:max-md:w-full xl:w-[calc(50vw-32px*2)] xl:items-center 2xl:w-[calc(1440px/2-32px*2)]">
@@ -29,7 +29,7 @@
         </div>
         <div
           v-else-if="item.youtubeLink"
-          class="child:md:max-xl:w-[calc(100vw-64px*2)] relative flex items-center justify-center child:max-md:w-full xl:w-[calc(50vw-32px*2)] 2xl:w-[calc(1440px/2-32px*2)]"
+          class="child:md:max-xl:w-[calc(100vw-64px*2)] relative flex items-center justify-center child:max-md:w-full lg:w-[calc(50vw-32px*2)] 2xl:w-[calc(1440px/2-32px*2)]"
         >
           <HorizontalVideo
             :youtube-url="item.youtubeLink"
@@ -39,17 +39,17 @@
         </div>
         <div
           v-else-if="item.youtubeLink || item.image.video === null"
-          class="max-xl:h-360 child:md:max-xl:w-[calc(100vw-64px*2)]  relative flex justify-center child:mx-auto child:rounded-6 child:max-md:w-full xl:w-[calc(50vw-32px*2)] xl:items-center 2xl:w-[calc(1440px/2-32px*2)]"
+          class="relative flex h-[432px] justify-center lg:max-w-[50%]  xl:h-[519px] xl:items-center"
         >
           <img
             v-if="item.image"
-            class="aspect-[4/3] h-full rounded-8 border border-blue-dark/20 object-cover shadow"
+            class=" aspect-[3/2] h-full rounded-8 border border-blue-dark/20 object-cover shadow lg:aspect-[4/3]"
             :src="item.image.url"
           >
         </div>
         <div
           v-if="item.badge"
-          class="mx-16 mt-16 flex h-32 w-max items-center gap-8 self-start overflow-hidden rounded bg-[#21BCA5]/20 px-16 py-8 text-[#13B59D] md:self-center xl:hidden xl:h-0"
+          class="mx-16 mt-16 flex h-32 w-max items-center gap-8 self-start overflow-hidden rounded bg-[#21BCA5]/20 px-16 py-8 text-[#13B59D] sm:self-center lg:hidden lg:h-0"
         >
           <svg
             viewBox="0 0 18 18"
@@ -69,24 +69,24 @@
         </div>
 
         <div
-          class="content !px-16 pt-64 md:items-center md:py-80 xl:flex-1 xl:!px-0 xl:py-96"
+          class="content mt-64 flex flex-col justify-center !px-16 lg:mt-0 lg:flex-1 lg:!px-0"
         >
           <div
-            class="mb-24 flex w-max md:mx-auto md:items-center xl:mx-unset xl:w-full"
+            class="mb-24 flex w-max sm:mx-auto sm:items-center lg:mx-unset lg:w-full"
             :class="{
-              'flex flex-col-reverse flex-wrap justify-between gap-16 sm:flex-row xl:justify-between':
+              'flex flex-col-reverse flex-wrap justify-between gap-16 sm:flex-row lg:justify-between':
                 item.badge,
             }"
           >
             <img
               v-if="item.logo"
               :src="item.logo.url"
-              class="flex max-h-40 w-max flex-col-reverse flex-wrap justify-between gap-16 sm:flex-row md:mx-auto md:items-center xl:mx-unset xl:justify-between"
+              class="flex max-h-40 w-max flex-col-reverse flex-wrap justify-between gap-16 sm:mx-auto sm:flex-row lg:mx-unset lg:items-center lg:justify-between"
               alt=""
             >
             <div
               v-if="item.badge"
-              class="hidden h-32 w-max items-center gap-8 rounded bg-[#21BCA5]/20 px-16 py-8 text-[#13B59D] xl:flex"
+              class="hidden h-32 w-max items-center gap-8 rounded bg-[#21BCA5]/20 px-16 py-8 text-[#13B59D] lg:flex"
             >
               <svg
                 viewBox="0 0 18 18"
@@ -107,16 +107,17 @@
           </div>
 
           <h3
-            class="max-w-screen-2xl font-semibold md:mx-auto md:text-center xl:mx-unset xl:text-left"
+            class="max-w-screen-2xl font-semibold sm:mx-auto sm:text-center lg:mx-unset lg:text-left"
           >
             {{ item.headline }}
           </h3>
-          <div class="mt-8 text-12 font-bold tracking-wide text-blue-dark/60 md:text-center xl:text-left">
+          <div class="mt-8 text-12 font-bold tracking-wide text-blue-dark/60 sm:text-center lg:text-left">
             {{ item.label }}
           </div>
-          <StructuredText class="prose mt-32 max-w-screen-2xl text-blue/60 md:mx-auto md:text-center xl:mx-unset xl:text-left" :data="item.description" />
+          <StructuredText class="prose mt-32 max-w-screen-2xl text-blue/60 sm:mx-auto sm:text-center lg:mx-unset lg:text-left" :data="item.description" />
           <div
-            class="mt-24 flex flex-col flex-wrap items-start justify-start gap-24 xs:flex-row xs:items-center md:justify-center xl:justify-start"
+            v-if="item.hasButton || item.hasSecondaryButton"
+            class="mt-24 flex flex-col flex-wrap items-start justify-start gap-24 xs:flex-row xs:items-center sm:justify-center lg:justify-start"
           >
             <TheLink
               v-if="item.hasButton"
