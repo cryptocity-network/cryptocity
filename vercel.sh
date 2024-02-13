9fb56115d01bb8319f93f79d1c327cf6ff8127b7
Jon Snow <jonny96snow@googlemail.com>
2024-02-13 17:23:27 +0000

    remove existing sandbox check

--- vercel.sh
+++ vercel.sh
@@ -20,19 +20,17 @@ else
     destination_env="preview-$VERCEL_GIT_PULL_REQUEST_ID"
     echo $FULL_ACCESS
 
-    if echo "$json_output" | jq -r '.[] | .id' | grep -q "^$destination_env$"; then
-        echo "Environment: $destination_env already exists"
-        yarn datocms migrations:run --source=$destination_env --in-place --api-token=$FULL_ACCESS
-        export DATO_ENVIRONMENT="preview-$VERCEL_GIT_PULL_REQUEST_ID"
-    else
-        echo "Environment: $destination_env doesn't exist"
-        primary_env=$(yarn datocms environments:primary --log-level=NONE --api-token=$FULL_ACCESS)
-        echo $primary_env
-        echo $destination_env
-        # yarn datocms migrations:run --source=$primary_env --destination=$destination_env --fast-fork --api-token=$FULL_ACCESS
-        yarn datocms migrations:run --destination=$destination_env --fast-fork --api-token=$FULL_ACCESS
-        export DATO_ENVIRONMENT="preview-$VERCEL_GIT_PULL_REQUEST_ID"
-    fi
+    # if echo "$json_output" | jq -r '.[] | .id' | grep -q "^$destination_env$"; then
+    #     echo "Environment: $destination_env already exists"
+    #     yarn datocms migrations:run --source=$destination_env --in-place --api-token=$FULL_ACCESS
+    #     export DATO_ENVIRONMENT="preview-$VERCEL_GIT_PULL_REQUEST_ID"
+    # else
+    #     echo "Environment: $destination_env doesn't exist"
+    #     primary_env=$(yarn datocms environments:primary --log-level=NONE --api-token=$FULL_ACCESS)
+    #     # yarn datocms migrations:run --source=$primary_env --destination=$destination_env --fast-fork --api-token=$FULL_ACCESS
+    # fi
+    yarn datocms migrations:run --destination=$destination_env --fast-fork --api-token=$FULL_ACCESS
+    export DATO_ENVIRONMENT="preview-$VERCEL_GIT_PULL_REQUEST_ID"
     yarn build
     yarn preview
 fi
