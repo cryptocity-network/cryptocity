b911ba90a1f9678001491d5e3f0fd8ecbe90beb6
Jon Snow <jonny96snow@googlemail.com>
2024-02-15 12:44:44 +0000

    Add teleport to map fullscreen

--- components/datocms-blocks/HeroSection.vue
+++ components/datocms-blocks/HeroSection.vue
@@ -5,20 +5,22 @@
     :overlaps-next-section="false"
     class="hero"
   >
-    <img
-      v-if="data.image"
-      :src="data.image.url"
-      class="mx-auto mb-40 !max-h-[5rem] !max-w-[90vw] px-32 md:px-64 lg:!max-h-[7rem] lg:!max-w-[272px] lg:!px-0"
-      alt=""
-    >
-    <div class="mx-auto max-w-screen-2xl px-64">
-      <HeadlineSection
-        :label="data.label"
-        :headline="data.headline"
-        :subline="data.subline"
-        :link="data.simpleLink"
-        :has-link="data.hasLink"
-      />
+    <div>
+      <img
+        v-if="data.image"
+        :src="data.image.url"
+        class="mx-auto mb-40 !max-h-[5rem] !max-w-[90vw] px-32 sm:px-64 lg:!max-h-[7rem] lg:!max-w-[272px] lg:!px-0"
+        alt=""
+      >
+      <div class="mx-auto max-w-screen-2xl">
+        <HeadlineSection
+          :label="data.label"
+          :headline="data.headline"
+          :subline="data.subline"
+          :link="data.simpleLink"
+          :has-link="data.hasLink"
+        />
+      </div>
     </div>
   </BlockWrapper>
 </template>
--- components/datocms-blocks/Iframe.vue
+++ components/datocms-blocks/Iframe.vue
@@ -8,58 +8,60 @@
     class="!pt-96"
   >
     <div class="iframe-wrapper aspect-[3/3.5] w-full sm:aspect-video">
-      <div
-        class="iframe-container size-full"
-        :class="{
-          'size-screen fixed left-0 top-0': fullscreen,
-          'relative': !fullscreen
-        }"
-      >
-        <div v-if="!loaded" class="absolute left-0 top-0 flex size-full flex-col items-center justify-center rounded-6 border-2 border-gray/60 bg-white">
-          <h3 class="mb-4">
-            Looks like something went wrong
-          </h3>
-          <p class="mb-20">
-            Click here to see the map
-          </p>
-          <TheLink
-            :url="data.iframe"
-            :is-external="true"
-            variant="info"
-            text="View Map"
+      <Teleport :disabled="!fullscreen" to="overlay">
+        <div
+          class="iframe-container size-full"
+          :class="{
+            'size-screen fixed left-0 top-0': fullscreen,
+            'relative': !fullscreen
+          }"
+        >
+          <div v-if="!loaded" class="absolute left-0 top-0 flex size-full flex-col items-center justify-center rounded-6 border-2 border-gray/60 bg-white">
+            <h3 class="mb-4">
+              Looks like something went wrong
+            </h3>
+            <p class="mb-20">
+              Click here to see the map
+            </p>
+            <TheLink
+              :url="data.iframe"
+              :is-external="true"
+              variant="info"
+              text="View Map"
+            />
+          </div>
+          <iframe
+            v-if="data.iframe"
+            class="absolute left-0 top-0 z-50 size-full rounded-6 shadow"
+            :src="data.iframe"
+            @load="triggerLoad"
           />
+          <transition name="fade" mode="out-in">
+            <button
+              v-if="loaded"
+              class="iframe-button grid-row-2 z-[999] grid size-32 cursor-pointer grid-cols-2 rounded-full border-1 border-gray bg-white p-4 shadow-banner"
+              :class="{
+                'absolute': !fullscreen,
+                'fixed bottom-120 right-24 z-[999] md:bottom-200': fullscreen
+              }"
+              @click="fullscreen = !fullscreen"
+            >
+              <ArrowExternal
+                class="col-start-2 m-1 w-6 self-end "
+                :class="{
+                  'rotate-180': fullscreen
+                }"
+              />
+              <ArrowExternal
+                class=" m-1 w-6 self-start justify-self-end"
+                :class="{
+                  'rotate-180': !fullscreen
+                }"
+              />
+            </button>
+          </transition>
         </div>
-        <iframe
-          v-if="data.iframe"
-          class="absolute left-0 top-0 z-50 size-full rounded-6 shadow"
-          :src="data.iframe"
-          @load="triggerLoad"
-        />
-        <transition name="fade" mode="out-in">
-          <button
-            v-if="loaded"
-            class="iframe-button grid-row-2 z-[999] grid size-32 cursor-pointer grid-cols-2 rounded-full border-1 border-gray bg-white p-4 shadow-banner"
-            :class="{
-              'absolute': !fullscreen,
-              'fixed bottom-120 right-24 z-[999] md:bottom-200': fullscreen
-            }"
-            @click="fullscreen = !fullscreen"
-          >
-            <ArrowExternal
-              class="col-start-2 m-1 w-6 self-end "
-              :class="{
-                'rotate-180': fullscreen
-              }"
-            />
-            <ArrowExternal
-              class=" m-1 w-6 self-start justify-self-end"
-              :class="{
-                'rotate-180': !fullscreen
-              }"
-            />
-          </button>
-        </transition>
-      </div>
+      </Teleport>
     </div>
   </BlockWrapper>
 </template>
