3931fc13ccd637f6d5e2549529d21b482277e4f1
Jon Snow <jonny96snow@googlemail.com>
2024-02-15 15:03:28 +0000

    add error page

---
+++ pages/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="bg-gray py-80">
+    <div class="prose">
+      <h1>Seems like you've found an error</h1>
+      <TheLink
+        url="/"
+        text="Return home"
+      />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
