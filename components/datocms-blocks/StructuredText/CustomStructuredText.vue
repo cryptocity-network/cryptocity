66f04af9155b58d0d6f7c84f0e62308edbc80c0c
Jon Snow <jonny96snow@googlemail.com>
2024-04-16 08:09:29 +0100

    Update or case state

--- components/datocms-blocks/StructuredText/CustomStructuredText.vue
+++ components/datocms-blocks/StructuredText/CustomStructuredText.vue
@@ -4,12 +4,17 @@
     :render-link-to-record="structuredTextLinkRenderer"
     :render-inline-record="renderInlineRecord"
     :data="data"
+    :render-block="renderBlock"
+    :custom-node-rules="customNodeRules"
   />
 </template>
 
 <script lang="ts" setup>
-import { StructuredText } from 'vue-datocms'
+import { StructuredText, renderNodeRule } from 'vue-datocms'
+import { isCode } from 'datocms-structured-text-utils'
+
 import type { StructuredTextDocument, RenderInlineRecordContext } from 'vue-datocms'
+import ImageWithCaption from '~/components/datocms-blocks/StructuredText/ImageWithCaption.vue'
 import structuredTextLinkRenderer from '@/composables/structuredTextLinkRenderer'
 defineProps({
   data: {
@@ -27,6 +32,26 @@ const renderInlineRecord = ({ record }: RenderInlineRecordContext) => {
       return h('a', { href: '/', innerHTML: title })
   }
 }
+const renderBlock = (record: any) => {
+  switch (record.record._modelApiKey) {
+    case 'image':
+      return h(ImageWithCaption, {
+        data: record.record
+      })
+    default:
+      return null
+  }
+}
+
+const customNodeRules = [
+  renderNodeRule(isCode, ({ adapter: { renderNode: h }, node }) => {
+    return h(
+      'pre',
+      {},
+      [h('code', { class: `language-${node.language}` }, node.code)]
+    )
+  })
+]
 </script>
 
 <style>
