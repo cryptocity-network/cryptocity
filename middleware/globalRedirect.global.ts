4c9263a8522b779c152d8ed15bfd82effe0f79e4
Jon Snow <jonny96snow@googlemail.com>
2024-04-18 12:04:39 +0100

    redirect /home on region sites

--- middleware/globalRedirect.global.ts
+++ middleware/globalRedirect.global.ts
@@ -1,11 +1,15 @@
 export default defineNuxtRouteMiddleware((to) => {
   if (useRuntimeConfig().public.IS_GLOBAL_SITE && (!to.path.includes('/home') && !to.path.includes('/contact'))) {
     const useLocale = useLocalePath()
-    console.log('redirect')
     return navigateTo(useLocale('/home'))
   }
   // No cities index page so redirect back to home
   if (to.path.includes('/cities') && !to.params.city) {
+    return navigateTo('/')
+  }
+
+  // Not global and goes to /home redirect to '/'
+  if (!useRuntimeConfig().public.IS_GLOBAL_SITE && to.path.includes('/home')) {
     return navigateTo('/')
   }
 
