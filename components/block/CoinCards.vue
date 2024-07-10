161883412e91faf611bc8e1cf1e5e091aeaa6942
Jon Snow <jonny96snow@googlemail.com>
2024-07-10 12:47:26 +0100

    bugfix: remove one element from coin cards

--- components/block/CoinCards.vue
+++ components/block/CoinCards.vue
@@ -10,7 +10,7 @@
       <ul
         ref="scroller"
         role="list"
-        class="relative flex w-full items-stretch justify-start gap-16 overflow-x-auto !px-32 pb-20 pt-6 md:flex-col md:items-start md:justify-center md:pb-40 xl:flex-row "
+        class="relative flex w-full items-stretch justify-start gap-16 overflow-x-auto !px-32 pb-64 pt-6 md:flex-col md:items-start md:justify-center md:pb-40 xl:flex-row "
       >
         <li
           v-for="(coinData, x) in data"
@@ -19,22 +19,16 @@
           data-location
           class="block max-w-screen-xs shrink-0 snap-center snap-always rounded-8 bg-white shadow md:flex md:w-full md:max-w-none md:shrink xl:block xl:max-w-[400px]"
         >
-          <!-- :class="x === '2' ? 'h-[560px]' : 'h-[530px]'" -->
-          <!-- <Location class="h-104" /> -->
-          <!-- <div class="relative h-auto grow overflow-hidden rounded-4"> -->
-          <!-- <img class="h-full object-fill" src="/static/map-business-background.png" alt=""> -->
           <div class="relative mx-6 mt-6 flex items-center rounded-6 bg-gray p-56 md:mb-6 md:mr-0 md:w-1/2 xl:mb-0 xl:mr-6 xl:w-auto ">
             <DatoImage :image="coinData.coinImage" class="mx-auto h-160 object-center" />
           </div>
-          <div class="grid w-full grid-cols-1 grid-rows-[min-content,min-content,min-content] 2xl:grid-cols-2 2xl:grid-rows-[min-content,min-content]">
+          <div class="grid w-full grid-cols-1 grid-rows-2">
             <div
-              v-for="(description, j) in coinData.coinDescriptions"
+              v-for="(description, j) in [coinData.coinDescriptions[0], coinData.coinDescriptions[2]]"
               :key="description.id"
-              class="flex flex-col items-center justify-center border-gray p-24 "
+              class="flex flex-col items-center justify-center border-gray p-24"
               :class="{
-                'border-t-2 2xl:border-t-0': j === 1,
-                'border-t-2 2xl:col-span-2': j == 2,
-                '2xl:border-r-2': j == 0
+                'border-t-2': j === 1
               }"
             >
               <DatoImage :image="description.icon" />
