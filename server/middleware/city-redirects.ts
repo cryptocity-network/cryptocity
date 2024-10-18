3912681ef6d6dd73192a2e10bcb79e32a1e5b186
onmax <maximogarciamtnez@gmail.com>
2024-10-18 15:02:26 +0200

    lint

--- server/middleware/city-redirects.ts
+++ server/middleware/city-redirects.ts
@@ -1,22 +1,22 @@
 export default defineEventHandler(async (event) => {
-    if (!useRuntimeConfig().REDIRECT_RULES) return
+  if (!useRuntimeConfig().REDIRECT_RULES) { return }
 
-    const url = getRequestURL(event)
+  const url = getRequestURL(event)
 
-    /**
+  /**
      * This expects a value of this format:
      * `targetdomain.com,citydomain1.com:cityname1,citydomain2.com:cityname2`
      */
-    const [targetdomain, ...redirects] = useRuntimeConfig().REDIRECT_RULES.split(',')
+  const [targetdomain, ...redirects] = useRuntimeConfig().REDIRECT_RULES.split(',')
 
-    const defaultLocale = useRuntimeConfig().public.DATO_DEFAULT_LOCALE || 'en'
+  const defaultLocale = useRuntimeConfig().public.DATO_DEFAULT_LOCALE || 'en'
 
-    for (const redirect of redirects) {
-        const [citydomain, cityname] = redirect.split(':')
+  for (const redirect of redirects) {
+    const [citydomain, cityname] = redirect.split(':')
 
-        if (url.hostname === citydomain.trim()) {
-            await sendRedirect(event, `https://${targetdomain.trim()}/${defaultLocale}/cities/${cityname.trim()}`, 301)
-            break;
-        }
+    if (url.hostname === citydomain.trim()) {
+      await sendRedirect(event, `https://${targetdomain.trim()}/${defaultLocale}/cities/${cityname.trim()}`, 301)
+      break
     }
+  }
 })
