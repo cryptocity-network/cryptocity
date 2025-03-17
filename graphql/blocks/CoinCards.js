d2d6c2854fad668df217fe7cb937cb90caf92d13
Jon Snow <jonny96snow@googlemail.com>
2025-03-17 15:02:45 +0100

    Fix: Remove coin card request

--- graphql/blocks/CoinCards.js
+++ graphql/blocks/CoinCards.js
@@ -1,18 +1,6 @@
-import ResponsiveImage from '../ResponsiveImage'
 import baseBlockValues from './blockValues'
 export default () => {
   return `
     ${baseBlockValues()}
-      coinImage {
-        ${ResponsiveImage()}
-      }
-      coinDescriptions {
-        text
-        id
-        comingSoon
-        icon {
-          ${ResponsiveImage()}
-        }
-      }
 `
 }
